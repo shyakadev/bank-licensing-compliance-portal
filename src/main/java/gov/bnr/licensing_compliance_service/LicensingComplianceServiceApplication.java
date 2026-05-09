bb68726af1ad6396821a57186aaba3a2d0300033
Shyaka L Tresor <shyakadev@gmail.com>
2026-05-09 22:51:12 +0200

    commit main application entry

---
+++ src/main/java/gov/bnr/licensing_compliance_service/LicensingComplianceServiceApplication.java
@@ -0,0 +1,13 @@
+package gov.bnr.licensing_compliance_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicensingComplianceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LicensingComplianceServiceApplication.class, args);
+    }
+
+}
